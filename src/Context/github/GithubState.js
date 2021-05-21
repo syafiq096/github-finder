5192e8a26cc7d1d151d5ea0c45fbeaba10695f5f
syafiq096 <83671187+syafiq096@users.noreply.github.com>
2021-05-21 15:06:13 +0800

    implement context api

---
+++ src/Context/github/GithubState.js
@@ -0,0 +1,74 @@
+import React, { useReducer } from "react";
+import axios from "axios";
+import GithubContext from "./githubContext";
+import Reducer from "./Reducer";
+import {
+  SEARCH_USER,
+  GET_USER,
+  GET_REPOS,
+  SET_ALERT,
+  SET_LOADING,
+  REMOVE_ALERT,
+} from "../types";
+
+function GithubState({ children }) {
+  const initialState = {
+    userList: [],
+    repos: [],
+    user: {},
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(Reducer, initialState);
+
+  // search users
+  const searchUsers = async (value) => {
+    setLoading();
+    await axios
+      .get(
+        `https://api.github.com/search/users?q=${value}&client_id=
+            ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      )
+      .then((res) => {
+        dispatch({ type: SEARCH_USER, payload: res.data.items });
+        setLoading();
+      });
+  };
+
+  // get user
+  // const getUserList = () => {
+  //     axios
+  //   .get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   )
+  //   .then((res) => {
+  //     setUser(res.data);
+  //     setLoading(false);
+  //   });
+  // }
+
+  // get repositories
+
+  // clear user list
+
+  // loading
+  const setLoading = () => {
+    dispatch({ type: SET_LOADING });
+  };
+
+  return (
+    <GithubContext.Provider
+      value={{
+        userList: state.userList,
+        repos: state.repos,
+        user: state.user,
+        loading: state.loading,
+        searchUsers,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+}
+
+export default GithubState;
---
+++ src/Context/github/Reducer.js
@@ -0,0 +1,26 @@
+import {
+    SEARCH_USER,
+    GET_USER,
+    GET_REPOS,
+    SET_ALERT,
+    SET_LOADING,
+    REMOVE_ALERT,
+} from "../types";
+  
+export default (state, action) => {
+    switch (action.type) {
+        case SEARCH_USER:
+            return {
+                ...state,
+                userList: action.payload,
+                loading: false,
+            }
+        case SET_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+        default:
+            return state;
+    }
+}
